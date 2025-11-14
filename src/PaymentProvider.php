8ed76551b23105a43e8e2a9d984fdf6ecfb269ec
Ramon Silva <silva018-mg@yahoo.com.br>
2025-11-14 00:22:10 -0300

    wip: add saloon dependency

--- src/PaymentProvider.php
+++ src/PaymentProvider.php
@@ -6,6 +6,7 @@ enum PaymentProvider: string
 {
     case Stripe = 'stripe';
     case Paddle = 'paddle';
+    case Asaas = 'asaas';
     case None = 'none';
 
     public function name(): string
@@ -13,13 +14,14 @@ enum PaymentProvider: string
         return match ($this) {
             self::Stripe => 'Stripe',
             self::Paddle => 'Paddle',
+            self::Asaas => 'Asaas',
             self::None => 'None',
         };
     }
 
     public function isValid(): bool
     {
-        return in_array($this, [self::Stripe, self::Paddle]);
+        return in_array($this, [self::Stripe, self::Paddle, self::Asaas]);
     }
 
     public function isStripe(): bool
@@ -32,11 +34,17 @@ enum PaymentProvider: string
         return $this === self::Paddle;
     }
 
+    public function isAsaas(): bool
+    {
+        return $this === self::Asaas;
+    }
+
     public function requiredComposerPackage(): string
     {
         return match ($this) {
             self::Stripe => 'laravel/cashier:"^15.0"',
             self::Paddle => 'laravel/cashier-paddle:"^2.0"',
+            self::Asaas => 'saloonphp/saloon:"^3.0"',
             self::None => '',
         };
     }
@@ -46,6 +54,7 @@ enum PaymentProvider: string
         return match ($this) {
             self::Stripe => 'Opcodes\Spike\Stripe\Subscription',
             self::Paddle => 'Opcodes\Spike\Paddle\Subscription',
+            self::Asaas => 'Opcodes\Spike\Asaas\Subscription',
             default => null,
         };
     }
@@ -55,6 +64,7 @@ enum PaymentProvider: string
         return match ($this) {
             self::Stripe => 'Opcodes\Spike\Stripe\SubscriptionItem',
             self::Paddle => 'Opcodes\Spike\Paddle\SubscriptionItem',
+            self::Asaas => 'Opcodes\Spike\Asaas\SubscriptionItem',
             default => null,
         };
     }
